df57d30bc8eb4b284010322647150807fb30ff51
Alexlion2308 <alexandreleconquerant@gmail.com>
2016-03-20 20:41:14 +0100

    Blank View Controller Added

---
+++ Fisu/ActivityTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityTableViewCell.swift
+//  Fisu
+//
+//  Created by vm mac on 20/03/2016.
+//  Copyright © 2016 Casati-Vasseur. All rights reserved.
+//
+
+import UIKit
+
+class ActivityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
